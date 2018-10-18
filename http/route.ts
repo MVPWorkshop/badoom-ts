b23aa35c589cd0f2b5ad5f0039390b99e03cfa78
Bogdan Habic <habicbogdan@gmail.com>
2018-10-18 11:51:38 +0200

    Code formatting.

--- http/route.ts
+++ http/route.ts
@@ -1,92 +1,92 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { HttpMethod } from './httpMethod';
 import { RouteHandler } from './routeHandler';
-import openAPI, { Doc } from '../docs/openAPI';
+import openAPI from '../docs/openAPI';
 import HttpResponse, { HttpResponseInterface } from './httpResponse';
 import 'reflect-metadata';
 
 export interface AdditionalOptions {
-    before?: RequestHandler[];
-    after?: RequestHandler[];
+  before?: RequestHandler[];
+  after?: RequestHandler[];
 }
 
 function isAdditionalOptions(additionalOption: AdditionalOptions | any[]): additionalOption is AdditionalOptions {
-    return additionalOption && ((<AdditionalOptions>additionalOption).before !== undefined || (<AdditionalOptions>additionalOption).after !== undefined);
+  return additionalOption && ((<AdditionalOptions>additionalOption).before !== undefined || (<AdditionalOptions>additionalOption).after !== undefined);
 }
 
 export function route(method: HttpMethod, path: string, additional?: any[] | AdditionalOptions) {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        let middlewares: RequestHandler[] = [];
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    let middlewares: RequestHandler[] = [];
 
-        const requestComponentPath = parseRequests(target, propertyKey);
-        parseResponses(target, propertyKey, method, path, requestComponentPath);
+    const requestComponentPath = parseRequests(target, propertyKey);
+    parseResponses(target, propertyKey, method, path, requestComponentPath);
 
-        if (!Reflect.getMetadata('wrapped_response_function', target, propertyKey)) {
-            descriptor.value = wrapResponse(descriptor.value);
-        }
+    if (!Reflect.getMetadata('wrapped_response_function', target, propertyKey)) {
+      descriptor.value = wrapResponse(descriptor.value);
+    }
 
-        if (isAdditionalOptions(additional)) {
-            if (additional.before) {
-                middlewares.push.apply(middlewares, additional.before);
-            }
+    if (isAdditionalOptions(additional)) {
+      if (additional.before) {
+        middlewares.push.apply(middlewares, additional.before);
+      }
 
-            middlewares.push(descriptor.value);
+      middlewares.push(descriptor.value);
 
-            if (additional.after) {
-                middlewares.push.apply(middlewares, additional.after);
-            }
-        } else {
-            middlewares = [descriptor.value];
-        }
+      if (additional.after) {
+        middlewares.push.apply(middlewares, additional.after);
+      }
+    } else {
+      middlewares = [descriptor.value];
+    }
 
-        RouteHandler.createRoute(target.constructor.name + '-' + propertyKey, method, path, middlewares);
+    RouteHandler.createRoute(target.constructor.name + '-' + propertyKey, method, path, middlewares);
 
-        return descriptor;
-    };
+    return descriptor;
+  };
 }
 
 function parseRequests(target: any, propertyKey: string): string | undefined {
-    const requestType = Reflect.getMetadata('design:open_api_request_type', target, propertyKey);
+  const requestType = Reflect.getMetadata('design:open_api_request_type', target, propertyKey);
 
-    if (!requestType) {
-        return;
-    }
+  if (!requestType) {
+    return;
+  }
 
-    return openAPI.addComponent(requestType);
+  return openAPI.addComponent(requestType);
 }
 
 function parseResponses(target: any, propertyKey: string, method: HttpMethod, path: string, requestComponentPath?: string): void {
-    const types: HttpResponseInterface[] = Reflect.getMetadata('design:open_api_types', target, propertyKey);
+  const types: HttpResponseInterface[] = Reflect.getMetadata('design:open_api_types', target, propertyKey);
 
-    if (!types || !types.length) {
-        return;
+  if (!types || !types.length) {
+    return;
+  }
+
+  types.forEach(responseType => {
+    const response = new responseType();
+
+    if (!response.code) {
+      return;
     }
 
-    types.forEach(responseType => {
-        const response = new responseType();
+    const code: number = response.code;
+    const componentPath: string = openAPI.addComponent(responseType);
 
-        if (!response.code) {
-            return;
-        }
-
-        const code: number = response.code;
-        const componentPath: string = openAPI.addComponent(responseType);
-
-        openAPI.addPath(path, method, code, componentPath, requestComponentPath);
-    });
+    openAPI.addPath(path, method, code, componentPath, requestComponentPath);
+  });
 }
 
 function wrapResponse(originalFunction: any): any {
-    return async function (req: Request, res: Response, next: NextFunction) {
-        const result: any = await originalFunction(req, res, next);
+  return async function (req: Request, res: Response, next: NextFunction) {
+    const result: any = await originalFunction(req, res, next);
 
-        if (result instanceof HttpResponse) {
-            res.status(result.code).json(result.toJson());
-            return;
-        }
+    if (result instanceof HttpResponse) {
+      res.status(result.code).json(result.toJson());
+      return;
+    }
 
-        if (result) {
-            res.json(result);
-        }
-    };
+    if (result) {
+      res.json(result);
+    }
+  };
 }
