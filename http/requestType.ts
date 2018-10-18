1192e79e2f5785731f264d6cf678cfa554ca4ed9
Dragan Obradovic <g4ndr4@gmail.com>
2018-10-18 18:03:08 +0200

    Added file upload support.

--- http/requestType.ts
+++ http/requestType.ts
@@ -10,6 +10,7 @@ export default function requestType(HttpRequest: HttpRequestInterface | any) {
     descriptor.value = async function (req: Request, res: Response, next: NextFunction) {
       const request = new HttpRequest();
       Object.assign(request, req.body);
+      Object.assign(request, req.files);
 
       const errors = await validate(request, {validationError: {target: false}});
 
